96e055582293725685085e8b68a4f8848b799677
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-18 15:17:23 -0400

    working on banner:

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Home from "./Home";
+import Header from "./Header";
 import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Home />
 
         {/* Home */}
