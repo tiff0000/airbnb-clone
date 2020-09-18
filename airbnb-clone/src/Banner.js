25d19dbd6ff0ba815f5bf43816ad7e132d725d8d
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-18 15:47:15 -0400

    working on date picker

--- airbnb-clone/src/Banner.js
+++ airbnb-clone/src/Banner.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Banner.css";
 import { Button } from "@material-ui/core";
+import Search from "./Search";
 
 function Banner() {
+  const [showSearch, setShowSearch] = useState(false);
+
   return (
     <div className="banner">
+      <div className="banner__search">
+        {showSearch && <Search />}
+        <Button
+          onClick={() => setShowSearch(!showSearch)}
+          className="banner__searchButton"
+          variant="outline"
+        >
+          Search Dates
+        </Button>
+      </div>
+
       <div className="banner__info">
         <h1>Get out and stretch your imagination</h1>
         <h5>
