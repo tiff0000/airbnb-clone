42fe53fe35f6df536444b1f41cfcf36a685b9404
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-18 14:36:55 -0400

    initialization of the hotel reservation app

---
+++ airbnb-clone/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDbGiDmg9vzjXwXGabbnAAl7noUChnoOXk",
+  authDomain: "airbnb-react-ec6e7.firebaseapp.com",
+  databaseURL: "https://airbnb-react-ec6e7.firebaseio.com",
+  projectId: "airbnb-react-ec6e7",
+  storageBucket: "airbnb-react-ec6e7.appspot.com",
+  messagingSenderId: "616262989880",
+  appId: "1:616262989880:web:eb07bd00baea9452600ecd",
+  measurementId: "G-78TVWT50RE"
+};
