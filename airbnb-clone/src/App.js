db995e0ba5e268143243484da21cea60cbe60448
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-18 16:58:42 -0400

    search page styling

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -3,22 +3,25 @@ import "./App.css";
 import Home from "./Home";
 import Header from "./Header";
 import Footer from "./Footer";
-import { BrowserRouter as Router } from "react-router-dom";
+import SearchPage from "./SearchPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
-        <Home />
+
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
         <Footer />
-
-        {/* Home */}
-
-        {/* Header */}
-        {/* Banner */}
-        {/* Search */}
-        {/* Cards */}
       </Router>
     </div>
   );
